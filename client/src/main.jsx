49a15b98b3af90c6c7a76408de6d1f8e7078503e
pascalecodes <pdelauna@gmail.com>
2024-10-06 23:02:28 -0700

    add persist store redux to main app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { store } from './redux/store';
+import { Provider } from "react-redux";
+// import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store= {store}>
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+      <App />
+    {/* </PersistGate> */}
+    </Provider>
+  </React.StrictMode>
+);
