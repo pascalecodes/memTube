6c4b0ea795f4538a92231fe7d987fb809257cd3a
pascalecodes <pdelauna@gmail.com>
2024-10-07 22:41:32 -0700

    add conditional to display signin only when no user logged in for menu

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -19,6 +19,7 @@ import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   flex: 1;
@@ -84,6 +85,9 @@ const Title = styled.h2`
 `;
 
 const Menu = ({darkMode, setDarkMode}) => {
+
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <Container>
     <Wrapper>
@@ -121,6 +125,8 @@ const Menu = ({darkMode, setDarkMode}) => {
           History
         </Item>
         <Hr/>
+        {!currentUser &&
+        <>
         <Login>
             Sign in to like videos, comment, and subscribe.
             <Link to="signin" style={{textDecoration:"none"}}>
@@ -131,6 +137,8 @@ const Menu = ({darkMode, setDarkMode}) => {
             </Link>
         </Login>
         <Hr/>
+        </>
+        }
         <Title>BEST OF MEMTUBE</Title>
         <Item>
           <LibraryMusicOutlinedIcon />
