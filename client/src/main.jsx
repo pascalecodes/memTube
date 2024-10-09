865d44aaa669d9aaa346aa61e889e006ac41d77f
pascalecodes <pdelauna@gmail.com>
2024-10-08 23:02:25 -0700

    fix refresh issue for maintaining state

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { Provider } from "react-redux";
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store= {store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    {/* </PersistGate> */}
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
