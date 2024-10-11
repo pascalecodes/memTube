a23f752cde729e98c6a04a80b99d8b6e0fbba6cc
pascalecodes <pdelauna@gmail.com>
2024-10-10 20:46:06 -0700

    start work to add signin with google using firebase auth

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,8 +3,8 @@ import React , { useState } from 'react'
 import styled from 'styled-components'
 import { useDispatch } from "react-redux";
 import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice";
-//import { auth, provider } from "../firebase";
-//import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
 //import { async } from "@firebase/util";
 //import { useNavigate } from "react-router-dom";
 
@@ -89,6 +89,28 @@ const SignIn = () => {
     }
   };
 
+  const signInWithGoogle = async () => {
+    dispatch(loginStart());
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        axios
+          .post("/auth/google", {
+            name: result.user.displayName,
+            email: result.user.email,
+            img: result.user.photoURL,
+          })
+          .then((res) => {
+            console.log(res)
+            dispatch(loginSuccess(res.data));
+            navigate("/")
+          });
+      })
+      .catch((error) => {
+        dispatch(loginFailure());
+      });
+  };
+
+
   return (
     <Container>
         <Wrapper>
@@ -97,6 +119,8 @@ const SignIn = () => {
         <Input placeholder="username" onChange={e=>setName(e.target.value)}/>
         <Input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
         <Button onClick={handleLogin}>Sign in</Button>
+        <Title>or</Title>
+        <Button onClick={signInWithGoogle}>Signin with Google</Button>
         <Title>or</Title>
         <Input placeholder="username" onChange={e=>setName(e.target.value)}/>
         <Input placeholder="email" onChange={e=>setEmail(e.target.value)}/>
