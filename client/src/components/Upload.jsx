48773ac1419db10fd0f05759d162930cc5fb4243
pascalecodes <pdelauna@gmail.com>
2024-10-26 20:40:39 -0700

    add video upload to gcp and save video to db

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState }  from 'react'
 import styled from "styled-components";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import app from "../firebase";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+
 
 const Container = styled.div`
   width: 100%;
@@ -78,7 +74,7 @@ const Upload = ({setOpen}) => {
     const [inputs, setInputs] = useState({});
     const [tags, setTags] = useState([]);
 
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
 
     const handleChange = (e) => {
         setInputs((prev) => {
@@ -89,39 +85,39 @@ const Upload = ({setOpen}) => {
     const handleTags = (e) => {
         setTags(e.target.value.split(","));
     };
-    // const uploadFile = (file, urlType) => {
-    //     const storage = getStorage(app);
-    //     const fileName = new Date().getTime() + file.name;
-    //     const storageRef = ref(storage, fileName);
-    //     const uploadTask = uploadBytesResumable(storageRef, file);
+    const uploadFile = (file, urlType) => {
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
     
-    //     uploadTask.on(
-    //       "state_changed",
-    //       (snapshot) => {
-    //         const progress =
-    //           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    //         urlType === "imgUrl" ? setImgPerc(Math.round(progress)) : setVideoPerc(Math.round(progress));
-    //         switch (snapshot.state) {
-    //           case "paused":
-    //             console.log("Upload is paused");
-    //             break;
-    //           case "running":
-    //             console.log("Upload is running");
-    //             break;
-    //           default:
-    //             break;
-    //         }
-    //       },
-    //       (error) => {},
-    //       () => {
-    //         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-    //           setInputs((prev) => {
-    //             return { ...prev, [urlType]: downloadURL };
-    //           });
-    //         });
-    //       }
-    //     );
-    // };
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            urlType === "imgUrl" ? setImgPerc(Math.round(progress)) : setVideoPerc(Math.round(progress));
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {},
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setInputs((prev) => {
+                return { ...prev, [urlType]: downloadURL };
+              });
+            });
+          }
+        );
+    };
 
     useEffect(() => {
       video && uploadFile(video , "videoUrl");
@@ -131,12 +127,12 @@ const Upload = ({setOpen}) => {
       img && uploadFile(img, "imgUrl");
     }, [img]);
   
-    // const handleUpload = async (e)=>{
-    //   e.preventDefault();
-    //   const res = await axios.post("/videos", {...inputs, tags})
-    //   setOpen(false)
-    //   res.status===200 && navigate(`/video/${res.data._id}`)
-    // }
+    const handleUpload = async (e)=>{
+      e.preventDefault();
+      const res = await axios.post("/api/videos", {...inputs, tags})
+      setOpen(false)
+      res.status===200 && navigate(`/video/${res.data._id}`)
+    }
 
 
   return (
@@ -145,13 +141,25 @@ const Upload = ({setOpen}) => {
             <Close onClick={()=>setOpen(false)}>X</Close>
             <Title>Upload a New Video</Title>
             <Label>Video:</Label>
-            <Input type="file" accept="video/*" onChange={e=>setVideo(e.target.files[0])}/>
-            <Input type="text" placeholder='Title' onChange={e=>setTitle(e.target.value)}/>
-            <Desc placeholder='Description' rows={8} onChange={e=>setDesc(e.target.value)}/>
+            {videoPerc > 0 ? ("Uploading:" + videoPerc + "%") : 
+            (<Input
+                type="file"
+                accept="video/*"
+                onChange={(e) => setVideo(e.target.files[0])}
+                />
+            )}
+            <Input type="text" placeholder='Title' name="title" onChange={handleChange}/>
+            <Desc placeholder='Description' rows={8} name="desc" onChange={handleChange}/>
             <Input type="text" placeholder='Seperate the tags with commas' onChange={handleTags}/>
             <Label>Image:</Label>
-            <Input type="file" accept="img/*" onChange={e=>setImg(e.target.files[0])}/>
-            <Button>Upload</Button>
+            {imgPerc > 0 ? ("Uploading:" + imgPerc + "%") : 
+            (<Input
+                type="file"
+                accept="image/*"
+                onChange={(e) => setImg(e.target.files[0])}
+                />
+            )}
+            <Button onClick={handleUpload}>Upload</Button>
         </Wrapper>
     </Container>
   )
