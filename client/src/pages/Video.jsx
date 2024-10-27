d296cfc04c261ff89b594756f49fc2afa49dc880
pascalecodes <pdelauna@gmail.com>
2024-10-26 20:47:35 -0700

    seperate recommendation to own component

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -61,9 +61,6 @@ const Hr = styled.hr`
   border: 0.5px solid ${({ theme }) => theme.soft};
 `;
 
-const Recommendation = styled.div`
-  flex: 2;
-`;
 const Channel = styled.div`
   display: flex;
   justify-content: space-between;
