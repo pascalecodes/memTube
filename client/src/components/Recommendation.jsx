6c7b760d5b767278180323699775e684ad563b51
pascalecodes <pdelauna@gmail.com>
2024-10-26 20:47:50 -0700

    add a recommendation component

---
+++ client/src/components/Recommendation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Container = styled.div`
+  flex: 2;
+`;
+
+const Recommendation = () => {
+  return (
+    <Container>
+      Recommendation
+    </Container>
+  )
+}
+
+export default Recommendation
