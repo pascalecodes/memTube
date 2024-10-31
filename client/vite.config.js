2b9d2ad876af814abab5aad3c93041931011ed35
pascalecodes <pdelauna@gmail.com>
2024-10-30 22:59:50 -0700

    comment change in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000/api/', // Your backend server URL
+        target: 'http://localhost:8000/api/', // backend server URL
         changeOrigin: true, // Changes the origin of the host header to the target URL
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix for the backend
       },
